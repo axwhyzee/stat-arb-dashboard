f11253b848b4db6042e02b4e648c1fcdaef40ca2
axwhyzee <siah_weehung@yahoo.com>
2023-05-03 12:24:27 +0800

    changed host

--- backend/main.py
+++ backend/main.py
@@ -192,4 +192,4 @@ async def schedule_interval():
     loop.create_task(query_interval())
 
 if __name__ == '__main__':
-    uvicorn.run('main:app', host='127.0.0.1', port=10000, reload=True)
+    uvicorn.run('main:app', host='0.0.0.0', port=10000, reload=True)
